7e4ed79505bdeef48a899c129ec87168de7bdb1f
Aravandaksa <ceo.fas@gmail.com>
2025-04-17 18:21:10 +0300

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,16 +1,18 @@
 // components/ui/button.tsx
-import React from 'react';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-}
+import React from "react";
 
-const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+export const Button = ({
+  children,
+  className = "",
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button {...props} className="px-4 py-2 bg-blue-500 text-white rounded">
+    <button
+      className={`px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition ${className}`}
+      {...props}
+    >
       {children}
     </button>
   );
 };
-
-export default Button;
